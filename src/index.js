c09394c9a1aea040c99e257d8cd7970e0d3827b8
Friedrich <friedrich.ten.hagen@googlemail.com>
2023-03-23 21:41:30 +0100

    set up gui and add pseudo code for main game loop

--- src/index.js
+++ src/index.js
@@ -5,6 +5,15 @@ import {ship} from "./ship.js"
 
 
 // main game loop
+// preset the ships
+// display human ships on field
+// add eventlistener to all computer fields
+    // make a human move (on computers field)computer.makeAMove([row, column])
+        // displayMoves on DOM (hits, misses)
+    // make a computer move( on the human field) human.makeARandomMove()
+
+
+
 
 const human = player("Friedrich")
 const computer = player("Computer")
@@ -22,10 +31,13 @@ computer.gameboard.placeShipX([1,0],4)
 computer.gameboard.placeShipX([5,3],3)
 computer.gameboard.placeShipY([0,5],2)
 
+// make human moves
+computer.makeARandomMove()
+computer.makeARandomMove()
+computer.makeARandomMove()
+computer.makeARandomMove()
+computer.makeARandomMove()
+
 console.log(human.gameboard.board, computer.gameboard.board)
 
 
-
-
-const testtttt = gameboard()
-window.testtttt = testtttt
